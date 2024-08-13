de680cd550366fb9a120fea6b9799cf22f9c874f
matheusbor <matheuslb2003@gmail.com>
2024-08-13 09:47:22 -0300

    Operações http

--- lib/repositories/product_repository.dart
+++ lib/repositories/product_repository.dart
@@ -1,3 +1,23 @@
+import 'dart:convert';
+
+import 'package:admin_cli/src/models/course.dart' ;
+import 'package:http/http.dart'as http;
+
 class ProductRepository{
-  
+  Future<Course> findByName(String name) async{//para so passar o nome no terminal e buscar o id
+  final response = await http.get(Uri.parse('http://localhost:8080/products?name=$name'));
+
+  if(response.statusCode != 200){
+
+    throw Exception();
+  }
+
+  final responseCurso = jsonDecode(response.body);
+
+  if (responseCurso.isEmpty){
+    throw Exception("Produto não encontrado");
+  }
+
+  return Course.fromMap(responseCurso.first);
+  }
 }
---
+++ lib/repositories/student_repository.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+
+import 'package:admin_cli/src/models/student.dart';
+import 'package:http/http.dart' as http;
+
+class StudentRepository{
+  Future<List<Student>> findAll() async{
+    final alunos = await http.get(Uri.parse("http://192.168.10.103:8080/students"));
+  
+  if(alunos.statusCode != 200){
+    throw Exception();
+  }
+
+  final alunosDados = jsonDecode(alunos.body);// as List; para o autocomplete funcionar - tirar depois para evitar comportamentos inesperados
+  //decode necessário porque é mais de um?
+
+  return alunosDados.map<Student>((student) => Student.fromMap(student)).toList(); 
+  
+  }
+
+  Future<Student> findById(int id) async{
+    final alunos = await http.get(Uri.parse("http://192.168.10.103:8080/students/$id"));
+
+    if(alunos.statusCode != 200){
+    throw Exception();
+  }
+
+    if(alunos.body == "{}"){//alunos.body.isEmpty json rest server não permite que body seja realmente vazio
+      throw Exception();
+    }
+
+  return Student.fromJson(alunos.body);
+  }
+
+  Future<void> insert(Student student) async{
+    final response = await http.post(Uri.parse("http://192.168.10.103:8080/students"), 
+    body: student.toJson(), 
+    headers: {
+      "content type": "application/json"
+    });//como eniamos um corpo temos que enviar algo informando que é um content type json, se não vai enviar como texto
+  
+    if(response.statusCode != 200){
+    throw Exception();
+  }
+
+  }
+
+  Future<void> update(Student student){}
+
+  Future<void> delete(int id){}
+
+
+  }
+}
