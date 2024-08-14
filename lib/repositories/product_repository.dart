2176cf5ba201d18c57c1cc7ae644037d1bef52d7
matheusbor <matheuslb2003@gmail.com>
2024-08-13 22:25:48 -0300

    Refatorando para usar package DIO

--- lib/repositories/product_repository.dart
+++ lib/repositories/product_repository.dart
@@ -1,23 +1,28 @@
 import 'dart:convert';
 
 import 'package:admin_cli/src/models/course.dart' ;
+import 'package:dio/dio.dart';
 import 'package:http/http.dart'as http;
 
 class ProductRepository{
   Future<Course> findByName(String name) async{//para so passar o nome no terminal e buscar o id
-  final response = await http.get(Uri.parse('http://localhost:8080/products?name=$name'));
+  
+  try{
+ 
+    final response = await Dio().get('http://localhost:8080/products?', queryParameters: {
+      "name": name
+    });
 
-  if(response.statusCode != 200){
-
-    throw Exception();
-  }
-
-  final responseCurso = jsonDecode(response.body);
-
-  if (responseCurso.isEmpty){
+  if (response.data.isEmpty){
     throw Exception("Produto não encontrado");
   }
 
-  return Course.fromMap(responseCurso.first);
+  return Course.fromMap(response.data.first);
+  }catch(e){
+      print(e);
+      throw Exception();
+
+  }
+  
   }
 }
--- lib/repositories/student_repository.dart
+++ lib/repositories/student_repository.dart
@@ -1,69 +1,75 @@
 import 'dart:convert';
 
 import 'package:admin_cli/src/models/student.dart';
+import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
 
 class StudentRepository{
   Future<List<Student>> findAll() async{
-    final alunos = await http.get(Uri.parse("http://192.168.10.103:8080/students"));
+
+    try{
+      final studentsResult = await Dio().get("http://192.168.10.103:8080/students");
+
+    return studentsResult.data.map<Student>((student) => Student.fromMap(student)).toList(); 
   
-  if(alunos.statusCode != 200){
-    throw Exception();
-  }
+    } catch(e){
+      print(e);
+      throw Exception();
+    }
 
-  final alunosDados = jsonDecode(alunos.body);// as List; para o autocomplete funcionar - tirar depois para evitar comportamentos inesperados
-  //decode necessário porque é mais de um?
-
-  return alunosDados.map<Student>((student) => Student.fromMap(student)).toList(); 
   
   }
 
   Future<Student> findById(int id) async{
-    final alunos = await http.get(Uri.parse("http://192.168.10.103:8080/students/$id"));
-
-    if(alunos.statusCode != 200){
-    throw Exception();
-  }
-
-    if(alunos.body == "{}"){//alunos.body.isEmpty json rest server não permite que body seja realmente vazio
+    
+    try{
+    final response = await Dio().get("http://192.168.10.103:8080/students/$id");
+   
+    if(response.data == null){
       print("Id não existe");
       throw Exception();
     }
 
-  return Student.fromJson(alunos.body);
+  return Student.fromMap(response.data);
+    } on DioException catch(e){
+      print(e);
+      throw Exception();
+    }
+
+    
   }
 
   Future<void> insert(Student student) async{
-    final response = await http.post(Uri.parse("http://192.168.10.103:8080/students"), 
-    body: student.toJson(), 
-    headers: {
-      "Content-Type": "application/json"
-    });//como eniamos um corpo temos que enviar algo informando que é um content type json, se não vai enviar como texto
-  
-    if(response.statusCode != 200){
+    
+    try {
+    await Dio().post("http://192.168.10.103:8080/students", data: student.toMap());
+} on DioException catch (e) {
+    print(e);
     throw Exception();
-  }
+}
+    
 
   }
 
   Future<void> update(Student student) async{
-    final response = await http.put(Uri.parse("http://192.168.10.103:8080/students/${student.id}"), 
-    body: student.toJson(), 
-    headers: {
-      "Content-Type": "application/json"
-    });//como eniamos um corpo temos que enviar algo informando que é um content type json, se não vai enviar como texto
   
-    if(response.statusCode != 200){
-    throw Exception();
-  }
+  try {
+  await Dio().put("http://192.168.10.103:8080/students/${student.id}", data: student.toMap());
+} on DioException catch (e) {
+  print(e);
+  throw Exception();
+}
+  
   }
 
   Future<void> delete(int id) async{
-    final response = await http.delete(Uri.parse("http://192.168.10.103:8080/students/$id"));
-  
-    if(response.statusCode != 200){
-    throw Exception();
-  }
+    
+  try {
+  await Dio().delete("http://192.168.10.103:8080/students/$id");
+} on DioException catch (e) {
+  print(e);
+  throw Exception();
+}
   }
 
   
